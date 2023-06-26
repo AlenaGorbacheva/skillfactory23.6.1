7428def4f228c224bded720a68bccb7cc3c91431
Алена <130080514+AlenaGorbacheva@users.noreply.github.com>
2023-06-26 14:09:05 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '6030457259:AAF28RI9zmOyGA8HfOYqEMrZIEmRPFYvf2k'
+
+keys = {
+    "привязь": "USDT",
+    "эфириум": "ETH",
+    "доллар": "USD",
+    'волны': 'WAVES',
+    'биткоин': 'BTC',
+}
